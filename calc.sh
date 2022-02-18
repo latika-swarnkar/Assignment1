9b4b2c85f3d5cc2e095d1062e82b388fd108a0b9
Latika Swarnkar <latika.swarnkar@innovaccer.com>
2022-02-18 13:56:24 +0530

    Added Equality functionality

--- calc.sh
+++ calc.sh
@@ -11,6 +11,7 @@ echo "2. Substraction"
 echo "3. Multiplication"
 echo "4. Division"
 echo "5. Modulus"
+echo "6. Check equality "
 read choice
 
 
@@ -25,6 +26,13 @@ case $choice in
    ;;
    "5") result=`expr $a % $b `
    ;; 
+   "6") if [ $a -eq $b ]
+        then
+            result="Both equal"
+        else
+            result="Both are not equal"
+        fi
+   ;;
 esac
 
 
